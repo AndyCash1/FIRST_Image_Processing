825a32d1873c180ffdebd86575cc95c1cfb59cd1
AndyCash1 <ascash1324@gmail.com>
2017-02-13 13:04:52 -0500

    updated startup script for 2 cameras

--- image_processing/startup_processing.sh
+++ image_processing/startup_processing.sh
@@ -1,23 +1,29 @@
 #!/bin/bash
 
-echo "Waiting 10s for things to startup"
-sleep 10
+echo "Waiting 1s for things to startup"
+sleep 1
 
 echo "Setting pixel format"
 export LD_PRELOAD=/usr/lib/arm-linux-gnueabihf/libv4l/v4l2convert.so
-sleep 2
+sleep 1
 
 echo "Changing camera settings"
-/usr/bin/v4l2-ctl -d /dev/video0 -c exposure_auto=1 -c exposure_absolute=$1 -c brightness=30 -c contrast=10
-sleep 2
+# Below is for the lifecam HD 3000
+#/usr/bin/v4l2-ctl -d /dev/video0 -c exposure_auto=1 -c exposure_absolute=$1 -c brightness=30 -c contrast=10
 
-echo "Making sure nothing is using the camera"
+# Below is for the Logitech C270
+/usr/bin/v4l2-ctl -d /dev/video0 -c exposure_auto=1 -c exposure_absolute=$1 -c brightness=0 -c contrast=255
+sleep 1
+/usr/bin/v4l2-ctl -d /dev/video1 -c exposure_auto=1 -c exposure_absolute=$1 -c brightness=0 -c contrast=255
+sleep 1
+
+echo "Making sure nothing is using the cameras"
 fuser -k /dev/video0
-sleep 5
+sleep 1
+fuser -k /dev/video1
+sleep 1
 
 echo "Launching image processing script"
 /usr/bin/python2.7 /home/ubuntu/Documents/FIRST_Image_Processing/image_processing/image_processing_2017frc.py
-sleep 1
 
 exit 0
-
